bd45677f4e23deb502af324a8e07990c2939ef48
Huy Ngo <minimaliststyle7@gmail.com>
2024-06-05 21:06:31 +0700

    edit file ignore

---
+++ test.js
@@ -0,0 +1,14 @@
+const maxArr = (arr) => Math.max(...arr)
+
+console.log(maxArr([1,2,3,4,5]))
+
+
+const funtionNumber1 = (param) => param
+
+const funtionNumber2 = (num) =>{
+    console.log(num * 2)
+}
+
+funtionNumber1(funtionNumber2(2))
+
+
